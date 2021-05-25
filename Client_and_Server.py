f5d26a5271bcdb2a77a00efac2cf6f69bfd26777
ZhangYixiSucceed <1395208387@qq.com>
2021-05-25 15:13:10 +0800

    new version

--- Client_and_Server.py
+++ Client_and_Server.py
@@ -15,6 +15,7 @@ import threading
 import time
 
 
+socket.setdefaulttimeout(5)
 
 
 class Ui_Demo(QtWidgets.QMainWindow):
@@ -24,10 +25,12 @@ class Ui_Demo(QtWidgets.QMainWindow):
         self._ui.setupUi(self)
         self._ui.tbnOpen.clicked.connect(self.OpenPort)
         self._ui.tbnSend.clicked.connect(self.Slot_tbnSend)
+        self._ui.tbnClear.clicked.connect(self.ClearShow)
         self.lESendData = ''
         self._ui.tbnClose.clicked.connect(self.Slot_CloseSocket)
 
         self.over_threading = False
+        self.IsOpen = False
 
 
         self.hostname = socket.gethostname()
@@ -35,6 +38,10 @@ class Ui_Demo(QtWidgets.QMainWindow):
         for add in self.self_ip_address:
             self._ui.cBPortName.addItem(add[4][0])
             print(add[4][0])
+        self._ui.tbnClose.setDisabled(True)
+
+        self._ui.lEServerPort.setText('51230')
+        self._ui.lEServerIP.setText('169.254.100.10')
 
 
     # def SendData(self):
@@ -60,38 +67,69 @@ class Ui_Demo(QtWidgets.QMainWindow):
 
 
         self.ADDR = (self.ServerIP, self.ServerPort)
-        self.tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建socket对象
+        self.tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)  # 创建socket对象
+
+        self.tcpCliSock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+
         self.tcpCliSock.connect(self.ADDR)  # 连接服务器
 
-        self.ClientSendData = threading.Thread(target=self.Slot_Receive, args=())
+        try:
+            self.ClientSendData = threading.Thread(target=self.Slot_Receive)
+        except Exception as e:
+            print(e)
         self.ClientSendData.start()
+        print('start %s' % self.ClientSendData.name)
+        print('当前活着的线程列表为:', threading.enumerate())
+
+        print('当前处于活动的线程个数为{} ,当前主线程为{},当前线程ID为{}'.format(threading.active_count(), threading.main_thread(),
+                                                          threading.get_ident()))
         self.over_threading = False
+        self._ui.tbnOpen.setDisabled(True)
+        self._ui.tbnClose.setDisabled(False)
+        # self.tcpCliSock.settimeout(5)
+
+        self.IsOpen = True
+
     def Slot_tbnSend(self):
 
-        self.lESendData = self._ui.lESend.text()
-        self.tcpCliSock.send(self.lESendData.encode('utf-8'))
-        return
+        if self.IsOpen == True:
+            self.lESendData = self._ui.lESend.text()
+            self.tcpCliSock.send(self.lESendData.encode('utf-8'))
+            return
 
     def Slot_Receive(self):
         self.BUFSIZE = 1024
-        while True:
-            self._ui.tEReceiveData.append(self.tcpCliSock.recv(self.BUFSIZE).decode('utf-8'))
-            if self.over_threading == True:
-                break
-        self.tcpCliSock.close()  # 关闭客户端
+        while self.over_threading == False:
+            try:
+                rx = self.tcpCliSock.recv(self.BUFSIZE).decode('utf-8')
+            except socket.timeout:
+                continue
+            # except TimeoutError:
+            #     continue
+            # except ConnectionError:
+            #     self.over_threading = True
+            #     break
+            self._ui.tEReceiveData.append(rx)
+
+
         return
 
     def Slot_CloseSocket(self):
 
+        self.IsOpen = False
         self.over_threading = True
+        self.ClientSendData.join()
+        self.tcpCliSock.close()  # 关闭客户端
+        self._ui.tbnOpen.setDisabled(False)
         ## time.sleep(5)
-        ## self.ClientSendData.join()
+
         ## self.tcpCliSock.close()  # 关闭客户端
 
 
 
         return
-    def ClosePort(self):
+    def ClearShow(self):
+        self._ui.tEReceiveData.setText('')
         return
 
 
